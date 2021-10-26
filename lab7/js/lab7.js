72a9eff79091aa2b2382523e7cef06fded314762
lejosanc <leolasanchez@gmail.com>
2021-10-25 17:44:16 -0700

    These changes were for lab 7 where we made an html with a popup function.

---
+++ lab7/js/lab7.js
@@ -0,0 +1,24 @@
+/**
+ * Author:  Sharon Chung and Leola Sanchez
+ * Created:   10.26.2021
+ * License: Public Domain.
+ **/
+ // Create a new function in your file, call it what you like, but choose wisely.
+
+function firstJunction() {
+      // In the function, declare a variable userName and use window.prompt() to get the user's name from the user. Here's some help (Links to an external site.).
+      var userName = window.prompt("Your name here please.");
+      console.log("userName =", userName);
+      //Spliting the string to array because you can't sort strings.
+      var nameArray = userName.split('');
+      console.log("nameArray =", nameArray);
+      // Sort the letters of the user's name and return those from the function.
+      var nameArraySort = nameArraySort.join('');
+      console.log("nameSorted=", nameSorted);
+      // Make sure your function uses return to return the results.
+      return nameSorted;
+    }
+
+      //output
+      document.writeln("Enter name here: ",
+            "Your name here please. </br>");
